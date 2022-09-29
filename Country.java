71d6b78c2fd4133346f5ac859804698861bc2392
Leonseo <101601213+Leonseo@users.noreply.github.com>
2022-09-29 12:22:16 +0300

    Create Country.java

---
+++ Country.java
@@ -0,0 +1,10 @@
+@Data
+public class Coutry {
+  
+  private Long id;
+  private String name;
+  private GovernmentForm governmentForm;
+  private Integer population;
+  
+  private List<City> cities;
+  
