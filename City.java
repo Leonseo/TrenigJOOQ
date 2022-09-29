946d75847f7fd9acbc15a4b80140c392c00df652
Leonseo <101601213+Leonseo@users.noreply.github.com>
2022-09-29 12:34:42 +0300

    Update City.java

--- City.java
+++ City.java
@@ -5,3 +5,21 @@ public clacc City {
   private Long countryId;
   private String name;
 }
+//Связь один-ко-многим. Country содержит множество связанных City. 
+//City содержит countryid
+
+create table countries 
+(
+  id              bigserail primary key,
+  name            varchar(255),
+  government_form varchar(255),
+  population      int
+  );
+
+create table cities
+(
+  id        bigserial primary key,
+  coutry_id bigith,
+  name      varchar(255)
+);
+
